27a43c4f018254d8c0d6f79ae06764ae58c24472
NGHIAVN1 <3120221424@ued.udn.vn>
2024-08-08 21:44:06 +0700

    Add .gitignore file

---
+++ models/restaurace.dart
@@ -0,0 +1,4 @@
+class FoodRestaurant {
+ late String numberPhone;
+ 
+}
---
+++ models/user.dart
@@ -0,0 +1,9 @@
+class User {
+  String phoneNumber;
+  String fullName;
+  String address;
+  User(
+      {required this.phoneNumber,
+      required this.fullName,
+      required this.address});
+}
